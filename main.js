ed8dd3a5c10d3a31c60db6ac9881d2244413c8fd
gmarquini <gabriel_marquini@hotmail.com>
2025-07-30 14:27:41 -0300

    deixando o botao mais baixo

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function changebc() {
   const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  console.log(randomColor);
   document.body.style.backgroundColor = randomColor;
 }
